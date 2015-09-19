c824086d7ca94bd50fbc6a669717c8e7dfa1f9ec
Robert Head <robert.head@gmail.com>
2015-09-19 12:43:29 -0700

    Deactivate 'confirmable' module.

--- app/models/garden_bed/user.rb
+++ app/models/garden_bed/user.rb
@@ -1,7 +1,14 @@
 module GardenBed
   class User < ActiveRecord::Base
-    devise :database_authenticatable, :registerable,
-           :recoverable, :rememberable, :trackable, :validatable,
-           :confirmable, :omniauthable
+    devise(
+      :database_authenticatable,
+      :registerable,
+      :recoverable,
+      :rememberable,
+      :trackable,
+      :validatable,
+      :omniauthable
+      # :confirmable
+    )
   end
 end
